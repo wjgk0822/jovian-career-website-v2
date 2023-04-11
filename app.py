b1193c442e3e0e3911ecf218f13c0ae737d9dc83
wjgk0822 <35884296+wjgk0822@users.noreply.github.com>
2023-04-11 14:31:02 +0000

    added connection to db

--- app.py
+++ app.py
@@ -1,44 +1,21 @@
 from flask import Flask, render_template, jsonify
+from database import load_jobs_from_db
+
 
 app = Flask(__name__)
 
-JOBS = [
-{
-  'id':1,
-  'title': 'Data Analyst',
-  'Location': 'Bengluru, India',
-  'salary': 'Rs. 10,00,000'
-},
-{
-  'id':2,
-  'title': 'Data Scientist',
-  'Location': 'Delhi, India',
-  'salary': 'Rs. 15,00,000'
-},
-{
-  'id':3,
-  'title': 'Frontend Engineer',
-  'Location': 'Remote'
-  
-},
-{
-  'id':4,
-  'title': 'Backend Engineer',
-  'Location': 'San Francisco, USA',
-  'salary': '$120,000'
-}
-]
 
 
 
 @app.route("/")
 def hello_world():
+  jobs=load_jobs_from_db()
   return render_template('home.html', 
-                         jobs=JOBS,     company_name='Jovian')
+                         jobs=jobs,     company_name='Jovian')
 
 @app.route("/api/jobs")
 def list_jobs():
-  return jsonfly(JOBS)
+  return jsonfly(jobs)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
